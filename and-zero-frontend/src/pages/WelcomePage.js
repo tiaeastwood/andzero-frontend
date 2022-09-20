9a3a06615449cbc591772ed67fd0c601523d0474
WillMash <wmashhadi@gmail.com>
2022-09-20 11:22:33 +0100

    tailwind and daisyui setup

--- and-zero-frontend/src/pages/WelcomePage.js
+++ and-zero-frontend/src/pages/WelcomePage.js
@@ -1,10 +1,11 @@
 
 
 function WelcomePage() {
-
     return (
         <>
-            <h1>Hello World</h1>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </>
     )
 }
