8bfcad05c44b93025002f0305373b5dd78a66bd3
Tia Eastwood <teastwood88@gmail.com>
2022-09-20 12:38:02 +0100

    header created

--- and-zero-frontend/src/App.js
+++ and-zero-frontend/src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
 import WelcomePage from './pages/WelcomePage';
+import Header from '../src/components/Header'
+
 function App() {
   return (
     <div className="App">
+      <Header/>
         <WelcomePage/>
     </div>
   );
---
+++ and-zero-frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+
+    return (
+        <header className="ANDRed pt-3 pb-3 pl-2 pr-2 flex justify-center text-5xl">
+            <h1><span class="font-bold">AND</span>zero</h1>
+        </header>
+    )
+}
+
+export default Header
