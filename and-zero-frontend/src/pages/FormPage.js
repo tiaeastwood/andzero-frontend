f7910598036f78171ed6792c37b3ae1d073f9a5e
Tia Eastwood <teastwood88@gmail.com>
2022-09-21 17:20:56 +0100

    display stats from actual data

--- and-zero-frontend/src/pages/FormPage.js
+++ and-zero-frontend/src/pages/FormPage.js
@@ -2,137 +2,144 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 function FormPage() {
-  const [coffeeCups, setCoffeeCups] = useState();
-  const [pledge, setPledge] = useState();
-  const [pledgeDate, setPledgeDate] = useState();
-  const [email, setEmail] = useState();
-  const [clubId, setClubId] = useState();
-  const [formResponse, setFormResponse] = useState();
+	const [coffeeCups, setCoffeeCups] = useState();
+	const [pledge, setPledge] = useState();
+	const [pledgeDate, setPledgeDate] = useState();
+	const [email, setEmail] = useState();
+	const [clubId, setClubId] = useState();
+	const [cupData, setCupData] = useState();
 
-  const navigation = useNavigate();
+	const navigation = useNavigate();
 
-  const canSubmit = coffeeCups && pledge && pledgeDate && email && clubId;
+	const canSubmit = coffeeCups && pledge && pledgeDate && email && clubId;
 
-  const submitForm = (e) => {
-    e.preventDefault();
+	const submitForm = (e) => {
+		e.preventDefault();
 
-    const url = "http://localhost:3001/pledge";
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "access-control-allow-origin": "*",
-      },
-      body: JSON.stringify({
-        email: email,
-        club: clubId,
-        date: pledgeDate,
-        cupsPledged: coffeeCups,
-      }),
-    };
-    fetch(url, requestOptions)
-      .then((response) => response.json())
-      .then(navigation("/Stats"))
-      .catch((error) => console.log("Form submit error", error));
-  };
+		const url = "http://localhost:3001/pledge";
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"access-control-allow-origin": "*",
+			},
+			body: JSON.stringify({
+				email: email,
+				club: clubId,
+				date: pledgeDate,
+				cupsPledged: coffeeCups,
+			}),
+		};
+		fetch(url, requestOptions)
+			.then((response) => response.json())
+			.then(navigation("/Stats"))
+			.catch((error) => console.log("Form submit error", error));
+	};
 
-  useEffect(() => {
-    console.log(
-      "coffeeCups",
-      coffeeCups,
-      "pledge",
-      pledge,
-      "email",
-      email,
-      "clubId",
-      clubId,
-      "pledgeDate",
-      pledgeDate
-    );
-  }, [coffeeCups, pledge, email, clubId, pledgeDate]);
+	const getCupsData = () => {
+		// const cupsUrl = "http://localhost:3001/cups";
+		// fetch(cupsUrl)
+		// 	.then((response) => response.json())
+		// 	.then((res) => console.log(res.data))
+		// 	.catch((error) => console.log("error getting data", error));
 
-  return (
-    <div id="form-container ">
-      <form className="p-12">
-        <label htmlFor="number">
-          On average, enter the number of coffees you have per day:
-        </label>
-        <input
-          name="number"
-          type="text"
-          placeholder="5"
-          className="input input-bordered input-secondary w-full mb-6"
-          onChange={(e) => setCoffeeCups(e.target.value)}
-        />
-        <label class="coffeePledgeLabel" htmlFor="radio">
-          Do you pledge to switch to a reusable coffee cup?
-        </label>
-        <div className="mb-5">
-          <label className="label cursor-pointer">
-            <div className="btnContainer flex justify-between">
-              <button
-                className="btn btn-primary ANDRed"
-                value={true}
-                onClick={(e) => setPledge(e.target.value)}
-              >
-                Yes
-              </button>
-              <button
-                className="btn btn-primary ANDRed"
-                value={false}
-                onClick={(e) => setPledge(e.target.value)}
-              >
-                No
-              </button>
-            </div>
-          </label>
-        </div>
+		fetch("http://localhost:3001/cups")
+			.then((response) => response.json())
+			.then((data) => {
+				//handle data
+				console.log(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-        <label htmlFor="number">From what date do you pledge to do this?</label>
-        <input
-          name="number"
-          type="date"
-          placeholder="5"
-          className="input input-bordered input-secondary w-full mb-6"
-          onChange={(e) => setPledgeDate(e.target.value)}
-        />
+	useEffect(() => {
+		getCupsData();
+	}, []);
 
-        <label htmlFor="club">Club</label>
-        <select
-          className="select select-secondary w-full mb-6"
-          onChange={(e) => setClubId(e.target.value)}
-        >
-          <option disabled defaultValue>
-            Pick your club
-          </option>
-          <option value="1">Wangari</option>
-          <option value="2">Woods</option>
-          <option value="3">Dekker</option>
-          <option value="4">Jemison</option>
-          <option value="5">Somerville</option>
-          <option value="6">Adams</option>
-        </select>
+	return (
+		<div id="form-container ">
+			<form className="p-12">
+				<label htmlFor="number">
+					On average, enter the number of coffees you have per day:
+				</label>
+				<input
+					name="number"
+					type="text"
+					placeholder="5"
+					className="input input-bordered input-secondary w-full mb-6"
+					onChange={(e) => setCoffeeCups(e.target.value)}
+				/>
+				<label class="coffeePledgeLabel" htmlFor="radio">
+					Do you pledge to switch to a reusable coffee cup?
+				</label>
+				<div className="mb-5">
+					<label className="label cursor-pointer">
+						<div className="btnContainer flex justify-between">
+							<button
+								className="btn btn-primary ANDRed"
+								value={true}
+								onClick={(e) => setPledge(e.target.value)}
+							>
+								Yes
+							</button>
+							<button
+								className="btn btn-primary ANDRed"
+								value={false}
+								onClick={(e) => setPledge(e.target.value)}
+							>
+								No
+							</button>
+						</div>
+					</label>
+				</div>
 
-        <label htmlFor="email">Email address</label>
-        <input
-          name="emil"
-          type="text"
-          placeholder="joe@joebloggs.com"
-          className="input input-bordered input-secondary w-full mb-6"
-          onChange={(e) => setEmail(e.target.value)}
-        />
+				<label htmlFor="number">From what date do you pledge to do this?</label>
+				<input
+					name="number"
+					type="date"
+					placeholder="5"
+					className="input input-bordered input-secondary w-full mb-6"
+					onChange={(e) => setPledgeDate(e.target.value)}
+				/>
 
-        <button
-          type="submit"
-          onClick={submitForm}
-          className="btn btn-primary ANDRed"
-          disabled={!canSubmit}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+				<label htmlFor="club">Club</label>
+				<select
+					className="select select-secondary w-full mb-6"
+					onChange={(e) => setClubId(e.target.value)}
+				>
+					<option disabled defaultValue>
+						Pick your club
+					</option>
+					<option value="1">Wangari</option>
+					<option value="2">Woods</option>
+					<option value="3">Dekker</option>
+					<option value="4">Jemison</option>
+					<option value="5">Somerville</option>
+					<option value="6">Adams</option>
+				</select>
+
+				<label htmlFor="email">Email address</label>
+				<input
+					name="emil"
+					type="text"
+					placeholder="joe@joebloggs.com"
+					className="input input-bordered input-secondary w-full mb-6"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+
+				<button
+					type="submit"
+					onClick={submitForm}
+					className="btn btn-primary ANDRed"
+					disabled={!canSubmit}
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default FormPage;
--- and-zero-frontend/src/pages/Stats.js
+++ and-zero-frontend/src/pages/Stats.js
@@ -1,142 +1,177 @@
 import { Bar, Pie } from "react-chartjs-2";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  ArcElement,
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	ArcElement,
 } from "chart.js";
 
 import React, { useState, useEffect } from "react";
 
-import Dishwasher from "/Users/will/Applications/and-zero-frontend/src/images/dishwasher.svg";
+import Dishwasher from "../images/dishwasher.svg";
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  ArcElement
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	ArcElement,
 );
 
 // ChartJS.defaults.global.defaultFontColor = "#fff";
 
 function Stats() {
-  const [chartData, setChartData] = useState({
-    datasets: [],
-  });
+	const [chartData, setChartData] = useState({
+		datasets: [],
+	});
 
-  const [chartOptions, setChartOptions] = useState({});
+	const [chartOptions, setChartOptions] = useState({});
+	const [cupsData, setCupsData] = useState({});
+	const [clubsList, setClubsList] = useState([]);
 
-  useEffect(() => {
-    setChartData({
-      labels: ["Woods", "Wangari ", "Adams", "Dekker", "Somerville", "Jemison"],
-      datasets: [
-        {
-          label: "Who is the judge?",
-          data: [12, 55, 14, 14, 56, 80],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-            "rgba(200, 200, 100, 1)",
-          ],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(200, 200, 100, 1)",
-          ],
-        },
-      ],
-    });
-    setChartOptions({
-      responsive: true,
-      plugins: {
-        legend: {
-          labels: {
-            fontColor: "#fff",
-            font: {
-              size: 20,
-            },
-          },
-          position: "top",
-        },
-        title: {
-          display: true,
-          text: "Cups saved between clubs",
-        },
-      },
-    });
-  }, []);
+	useEffect(() => {
+		setChartData({
+			labels: ["Woods", "Wangari ", "Adams", "Dekker", "Somerville", "Jemison"],
+			datasets: [
+				{
+					label: "Who is the judge?",
+					data: [12, 55, 14, 14, 56, 80],
+					borderColor: [
+						"rgba(255, 99, 132, 1)",
+						"rgba(54, 162, 235, 1)",
+						"rgba(255, 206, 86, 1)",
+						"rgba(75, 192, 192, 1)",
+						"rgba(153, 102, 255, 1)",
+						"rgba(255, 159, 64, 1)",
+						"rgba(200, 200, 100, 1)",
+					],
+					backgroundColor: [
+						"rgba(255, 99, 132, 0.2)",
+						"rgba(54, 162, 235, 0.2)",
+						"rgba(255, 206, 86, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(153, 102, 255, 0.2)",
+						"rgba(255, 159, 64, 0.2)",
+						"rgba(200, 200, 100, 1)",
+					],
+				},
+			],
+		});
+		setChartOptions({
+			responsive: true,
+			plugins: {
+				legend: {
+					labels: {
+						fontColor: "#fff",
+						font: {
+							size: 20,
+						},
+					},
+					position: "top",
+				},
+				title: {
+					display: true,
+					text: "Cups saved between clubs",
+				},
+			},
+		});
+	}, []);
 
-  return (
-    // <div className="hero min-h-screen">
-    //   <div className="mt-48" id="pie-chart">
-    //     <h1>Stats</h1>
-    //     <div>
-    //       <div className="mt-48">
-    //         <h1>Total ANDIs Pledged</h1>
-    //         <h2>63</h2>
-    //       </div>
-    //       <div>
-    //         <h1>Total Cups Saved</h1>
-    //         <h2>147</h2>
-    //       </div>
-    //     </div>
-    //     <Pie options={chartOptions} data={chartData} />
-    //     <h2>147</h2>
-    //   </div>
-    // </div>
+	const fetchClubs = async () => {
+		const clubUrl = "http://localhost:3001/clubs";
+		const response = await fetch(clubUrl);
+		if (!response.ok) {
+			throw new Error("Data could not be fetched");
+		}
+		const data = await response.json();
+		console.log("data", data);
+		setClubsList(data);
+		return data;
+	};
 
-    <div className="hero min-h-screen">
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content flex flex-col">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-2xl font-bold">
-            {" "}
-            Welcome Here's How You're Doing:
-          </h1>
-          <div className="flex text-center justify-evenly">
-            <div className="statBackground">
-              <p className="mb-5"> Total ANDis Pledged</p>
-              <p className="mb-5 statNumber"> 63</p>
-            </div>
-            <div>
-              <p className="mb-5"> Total Cups Saved</p>
-              <p className="mb- statNumber"> 147</p>
-            </div>
-          </div>
+	const getDataFromClubs = () => {
+		for (const club of clubsList) {
+			fetch(`http://localhost:3001/cups/${club.id}`)
+				.then((response) => response.json())
+				.then((data) => {
+					console.log("clubsList", data);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
 
-          <div>
-            <h1 className="mb-4">
-              Your Club Has Saved Enough Energy To Run the Dishwasher:
-            </h1>
-            <div className="flex justify-evenly items-center">
-              <h2 id="dishwasherX">7x</h2>
-              <img id="dishwasherImg" src={Dishwasher} alt="" />
-            </div>
-          </div>
+	const getCupsData = () => {
+		fetch("http://localhost:3001/cups")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("cupsdata", data);
+				setCupsData(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-          <div className=""></div>
-        </div>
-        <div id="pie-chart">
-          <Pie options={chartOptions} data={chartData} />
-        </div>
-      </div>
-    </div>
-  );
+	const calculateUsage = (cupsData.totalCupsSaved * 0.58 * 365) / 9.5;
+	const formattedNumber = Math.floor(calculateUsage);
+
+	useEffect(() => {
+		fetchClubs();
+		getCupsData();
+	}, []);
+
+	useEffect(() => {
+		if (clubsList) {
+			getDataFromClubs();
+		}
+	}, [clubsList]);
+
+	return (
+		<div className="hero min-h-screen">
+			<div className="hero-overlay bg-opacity-60"></div>
+			<div className="hero-content text-center text-neutral-content flex flex-col">
+				<div className="max-w-md">
+					<h1 className="mb-5 text-2xl font-bold">
+						{" "}
+						Welcome Here's How You're Doing:
+					</h1>
+					<div className="flex text-center justify-evenly">
+						<div className="statBackground">
+							<p className="mb-5"> Total ANDis Pledged</p>
+							<p className="mb-5 statNumber"> {cupsData.totalUsers}</p>
+						</div>
+						<div>
+							<p className="mb-5"> Total Cups Saved</p>
+							<p className="mb- statNumber"> {cupsData.totalCupsSaved}</p>
+						</div>
+					</div>
+
+					<div>
+						<h1 className="mb-4">
+							Your Club Has Saved Enough Energy To Run the Dishwasher:
+						</h1>
+						<div className="flex justify-evenly items-center">
+							<h2 id="dishwasherX">x {formattedNumber}</h2>
+							<img id="dishwasherImg" src={Dishwasher} alt="" />
+						</div>
+					</div>
+
+					<div className=""></div>
+				</div>
+				<div id="pie-chart">
+					<Pie options={chartOptions} data={chartData} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Stats;
