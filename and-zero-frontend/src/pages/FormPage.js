5ce1da4425a471d24ce919e4763a6092c4d73874
Tia Eastwood <teastwood88@gmail.com>
2022-09-22 17:17:42 +0100

    link random facts to actual data

--- and-zero-frontend/src/pages/FormPage.js
+++ and-zero-frontend/src/pages/FormPage.js
@@ -2,144 +2,158 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 function FormPage() {
-  const [coffeeCups, setCoffeeCups] = useState();
-  const [pledge, setPledge] = useState();
-  const [pledgeDate, setPledgeDate] = useState();
-  const [email, setEmail] = useState();
-  const [clubId, setClubId] = useState();
-  const [clubsList, setClubsList] = useState([]);
+	const [coffeeCups, setCoffeeCups] = useState();
+	const [pledgeDate, setPledgeDate] = useState();
+	const [email, setEmail] = useState();
+	const [clubId, setClubId] = useState();
+	const [clubsList, setClubsList] = useState([]);
+	const [showMsg, setShowMsg] = useState(false);
+	const [displayMessage, setDisplayMessage] = useState(
+		"Error: Please check form and try again.",
+	);
 
-  const navigation = useNavigate();
+	const navigation = useNavigate();
 
-  const canSubmit = coffeeCups && pledge && pledgeDate && email && clubId;
+	const canSubmit = coffeeCups && pledgeDate && email && clubId;
 
-  const submitForm = (e) => {
-    e.preventDefault();
+	const submitForm = (e) => {
+		e.preventDefault();
 
-    const url = "http://localhost:3001/pledge";
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "access-control-allow-origin": "*",
-      },
-      body: JSON.stringify({
-        email: email,
-        club: clubId,
-        date: pledgeDate,
-        cupsPledged: coffeeCups,
-      }),
-    };
-    fetch(url, requestOptions)
-      .then((response) => response.json())
-      .then(navigation("/Stats"))
-      .catch((error) => console.log("Form submit error", error));
-  };
+		const url = "http://localhost:3001/pledge";
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"access-control-allow-origin": "*",
+			},
+			body: JSON.stringify({
+				email: email,
+				club: clubId,
+				date: pledgeDate,
+				cupsPledged: coffeeCups,
+			}),
+		};
+		fetch(url, requestOptions)
+			.then((response) => response.json())
+			.then((res) => {
+				if (res.status === 400 || res.status === 404 || res.status === 401) {
+					setDisplayMessage(res.message);
+					setShowMsg(true);
+					setTimeout(() => {
+						setShowMsg(false);
+					}, 3000);
+				} else {
+					navigation("/Stats");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  const fetchClubs = async () => {
-    const clubUrl = "http://localhost:3001/clubs";
+	const fetchClubs = async () => {
+		const clubUrl = "http://localhost:3001/clubs";
 
-    const response = await fetch(clubUrl);
+		const response = await fetch(clubUrl);
 
-    if (!response.ok) {
-      throw new Error("Data could not be fetched");
-    }
+		if (!response.ok) {
+			throw new Error("Data could not be fetched");
+		}
 
-    return await response.json();
-  };
+		return await response.json();
+	};
 
-  useEffect(() => {
-    fetchClubs()
-      .then((clubsList) => {
-        setClubsList(clubsList);
-      })
-      .catch((error) => console.log(error.message));
-  }, []);
+	useEffect(() => {
+		fetchClubs()
+			.then((clubsList) => {
+				setClubsList(clubsList);
+			})
+			.catch((error) => console.log(error.message));
+	}, []);
 
-  return (
-    <div id="form-container">
-      <form className="p-12">
-        <label htmlFor="number">
-          On average, enter the number of coffees you have per day:
-        </label>
-        <input
-          name="number"
-          type="text"
-          placeholder="5"
-          className="input input-bordered input-secondary w-full mb-6"
-          onChange={(e) => setCoffeeCups(e.target.value)}
-        />
-        <label htmlFor="radio">
-          Do you pledge to switch to a reusable coffee cup?
-        </label>
-        <div className="mb-5">
-          <div id="pledgeContainer" className="flex items-between">
-            <button
-              className="rounded-lg ANDRed"
-              value={true}
-              onClick={(e) => setPledge(e.target.value)}
-            >
-              YES
-            </button>
+	return (
+		<div id="form-container" className="mt-8">
+			<form className="p-12">
+				<label htmlFor="number">
+					On average, enter the number of disposable coffee cups you have per
+					day:
+				</label>
+				<input
+					name="number"
+					type="text"
+					placeholder="5"
+					className="input input-bordered input-secondary w-full mb-6"
+					onChange={(e) => setCoffeeCups(e.target.value)}
+				/>
 
-            <button
-              className="p-3 rounded-lg ANDRed ml-2"
-              value={false}
-              onClick={(e) => setPledge(e.target.value)}
-            >
-              NO
-            </button>
-          </div>
-        </div>
+				{/* <div className="mb-5">
+					<div id="pledgeContainer" className="flex items-between">
+						<button className="rounded-lg ANDRed" value={true}>
+							YES
+						</button>
 
-        <label htmlFor="number">From what date do you pledge to do this?</label>
-        <input
-          name="number"
-          type="date"
-          placeholder="5"
-          className="input input-bordered input-secondary w-full mb-6"
-          onChange={(e) => setPledgeDate(e.target.value)}
-        />
+						<button className="p-3 rounded-lg ANDRed ml-2" value={false}>
+							NO
+						</button>
+					</div>
+				</div> */}
 
-        <label htmlFor="club">Club</label>
-        <select
-          className="select select-secondary w-full mb-6"
-          onChange={(e) => setClubId(e.target.value)}
-        >
-          <option disabled defaultValue>
-            Pick your club
-          </option>
-          {clubsList &&
-            clubsList.map((club, index) => {
-              return (
-                <option key={index} value={club.id}>
-                  {club.club}
-                </option>
-              );
-            })}
-        </select>
+				<label htmlFor="number">
+					From what date do you pledge to switch to a reusable coffee cup?
+				</label>
+				<input
+					name="number"
+					type="date"
+					placeholder="5"
+					className="input input-bordered input-secondary w-full mb-6"
+					onChange={(e) => setPledgeDate(e.target.value)}
+				/>
 
-        <label htmlFor="email">Email address</label>
-        <input
-          name="emil"
-          type="text"
-          placeholder="joe@joebloggs.com"
-          className="input input-bordered input-secondary w-full mb-6"
-          onChange={(e) => setEmail(e.target.value)}
-        />
+				<label htmlFor="club">Club</label>
+				<select
+					className="select select-secondary w-full mb-6"
+					onChange={(e) => setClubId(e.target.value)}
+				>
+					<option disabled defaultValue>
+						Pick your club
+					</option>
+					{clubsList &&
+						clubsList.map((club, index) => {
+							return (
+								<option key={index} value={club.id}>
+									{club.club}
+								</option>
+							);
+						})}
+				</select>
 
-        <button
-          type="submit"
-          onClick={submitForm}
-          id="submitBtn"
-          className="btn btn-primary ANDRed w-full"
-          disabled={!canSubmit}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+				<label htmlFor="email">Email address</label>
+				<input
+					name="email"
+					type="text"
+					placeholder="joe@joebloggs.com"
+					className="input input-bordered input-secondary w-full mb-6"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+
+				<button
+					type="submit"
+					onClick={submitForm}
+					id="submitBtn"
+					className="btn btn-primary ANDRed w-full"
+					disabled={!canSubmit}
+				>
+					Submit
+				</button>
+
+				{showMsg && (
+					<div className="alert-message">
+						<span>{displayMessage}</span>
+					</div>
+				)}
+			</form>
+		</div>
+	);
 }
 
 export default FormPage;
