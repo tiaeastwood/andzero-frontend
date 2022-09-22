0acf385f98ea5655eb2f2b03294f5c55aa1b52fa
WillMash <wmashhadi@gmail.com>
2022-09-22 16:30:52 +0100

    created a random fact feature

--- and-zero-frontend/src/pages/Stats.js
+++ and-zero-frontend/src/pages/Stats.js
@@ -1,193 +1,240 @@
 import { Pie } from "react-chartjs-2";
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
-	ArcElement,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
 } from "chart.js";
 
 import React, { useState, useEffect } from "react";
 
 import Dishwasher from "../images/dishwasher.svg";
+import Car from "../images/car.svg";
+import Forest from "../images/forest.svg";
 
 ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
-	ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
 );
 
 function Stats() {
-	const [chartData, setChartData] = useState({
-		datasets: [],
-	});
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
 
-	const [chartOptions, setChartOptions] = useState({});
-	const [cupsData, setCupsData] = useState({});
-	const [clubsList, setClubsList] = useState([]);
+  const [chartOptions, setChartOptions] = useState({});
+  const [cupsData, setCupsData] = useState({});
+  const [clubsList, setClubsList] = useState([]);
+  const [fact, setFact] = useState({});
 
-	const fetchClubs = async () => {
-		const clubUrl = "http://localhost:3001/clubs";
-		const response = await fetch(clubUrl);
-		if (!response.ok) {
-			throw new Error("Data could not be fetched");
-		}
-		const data = await response.json();
-		setClubsList(data);
-		return data;
-	};
+  const fetchClubs = async () => {
+    const clubUrl = "http://localhost:3001/clubs";
+    const response = await fetch(clubUrl);
+    if (!response.ok) {
+      throw new Error("Data could not be fetched");
+    }
+    const data = await response.json();
+    setClubsList(data);
+    return data;
+  };
 
-	const getDataFromClubs = async () => {
-		const arr = [];
-		try {
-			for (const club of clubsList) {
-				const response = await fetch(`http://localhost:3001/cups/${club.id}`);
+  const getDataFromClubs = async () => {
+    const arr = [];
+    try {
+      for (const club of clubsList) {
+        const response = await fetch(`http://localhost:3001/cups/${club.id}`);
 
-				if (!response.ok) {
-					throw new Error("Data could not be fetched");
-				}
+        if (!response.ok) {
+          throw new Error("Data could not be fetched");
+        }
 
-				const data = await response.json();
+        const data = await response.json();
 
-				const newItem = {
-					clubId: club.id,
-					clubName: club.club,
-					data: data.totalCupsSaved,
-				};
-				arr.push(newItem);
-			}
-			let names = [];
-			let stats = [];
+        const newItem = {
+          clubId: club.id,
+          clubName: club.club,
+          data: data.totalCupsSaved,
+        };
+        arr.push(newItem);
+      }
+      let names = [];
+      let stats = [];
 
-			for (let i = 0; i < arr.length; i++) {
-				names.push(arr[i].clubName);
-			}
+      for (let i = 0; i < arr.length; i++) {
+        names.push(arr[i].clubName);
+      }
 
-			for (let i = 0; i < arr.length; i++) {
-				stats.push(arr[i].data);
-			}
+      for (let i = 0; i < arr.length; i++) {
+        stats.push(arr[i].data);
+      }
 
-			setChartData({
-				labels: names,
-				datasets: [
-					{
-						label: "Who is the judge?",
-						data: stats,
-						borderColor: [
-							"rgba(255, 99, 132, 1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(200, 200, 100, 1)",
-						],
-						backgroundColor: [
-							"rgba(255, 99, 132, 0.2)",
-							"rgba(54, 162, 235, 0.2)",
-							"rgba(255, 206, 86, 0.2)",
-							"rgba(75, 192, 192, 0.2)",
-							"rgba(153, 102, 255, 0.2)",
-							"rgba(255, 159, 64, 0.2)",
-							"rgba(200, 200, 100, 1)",
-						],
-					},
-				],
-			});
+      setChartData({
+        labels: names,
+        datasets: [
+          {
+            label: "Who is the judge?",
+            data: stats,
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+              "rgba(200, 200, 100, 1)",
+            ],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(200, 200, 100, 1)",
+            ],
+          },
+        ],
+      });
 
-			setChartOptions({
-				responsive: true,
-				plugins: {
-					legend: {
-						labels: {
-							color: "white",
-							font: {
-								size: 18,
-							},
-						},
-					},
-					title: {
-						display: true,
-						color: "white",
-						text: "Cups saved between clubs",
-						font: {
-							size: 20,
-						},
-					},
-				},
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      setChartOptions({
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              fontColor: "#fff",
+              font: {
+                size: 20,
+              },
+            },
+            position: "top",
+          },
+          title: {
+            display: true,
+            text: "Cups saved between clubs",
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const getCupsData = () => {
-		fetch("http://localhost:3001/cups")
-			.then((response) => response.json())
-			.then((data) => {
-				setCupsData(data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+  const getCupsData = () => {
+    fetch("http://localhost:3001/cups")
+      .then((response) => response.json())
+      .then((data) => {
+        setCupsData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	const calculateUsage = (cupsData.totalCupsSaved * 0.58 * 365) / 9.5;
-	const formattedNumber = Math.floor(calculateUsage);
+  const dummyCupData = 1000;
 
-	useEffect(() => {
-		fetchClubs();
-		getCupsData();
-	}, []);
+  const calculateUsage = (dummyCupData * 0.58 * 365) / 9.5;
+  const formattedNumber = Math.floor(calculateUsage);
 
-	useEffect(() => {
-		if (clubsList) {
-			getDataFromClubs();
-		}
-	}, [clubsList]);
+  const treesNeeded = (dummyCupData * 110 * 365) / 25000;
+  const formattedTreesNeeded = Math.floor(treesNeeded);
+  console.log(`trees needed ${treesNeeded}`);
 
-	return (
-		<div className="hero min-h-screen">
-			<div className="hero-overlay bg-opacity-60"></div>
-			<div className="hero-content text-center text-neutral-content flex flex-col">
-				<div className="max-w-md">
-					<h1 className="mb-5 text-2xl font-bold">
-						{" "}
-						Welcome! Here's how you're doing:
-					</h1>
-					<div className="flex text-center justify-evenly">
-						<div className="statBackground">
-							<p>Total ANDis Pledged</p>
-							<p className="mb-5 statNumber"> {cupsData.totalUsers}</p>
-						</div>
-						<div>
-							<p> Total Cups Saved</p>
-							<p className="mb-5 statNumber"> {cupsData.totalCupsSaved}</p>
-						</div>
-					</div>
+  const milesEquivalent = dummyCupData * 365 * 0.59;
+  const formattedMilesEquivalent = Math.floor(milesEquivalent);
 
-					<div id="featured-stat" className="mb-5 p-5">
-						<h1 className="mb-5 text-2xl">You've saved enough energy to:</h1>
-						<div className="flex justify-evenly items-center">
-							<p className="text-xl">run the dishwasher</p>
-							<img id="dishwasherImg" src={Dishwasher} alt="dishwasher" />
-							<h2 className="text-xl">{formattedNumber} times!</h2>
-						</div>
-					</div>
-				</div>
-				<div id="pie-chart">
-					<Pie options={chartOptions} data={chartData} />
-				</div>
-			</div>
-		</div>
-	);
+  const factsArray = [formattedNumber, formattedTreesNeeded, treesNeeded];
+
+  const factBankObject = {
+    dishwasher: {
+      introText: "AND Has Saved Enough Energy To Run A Dishwasher:",
+      figure: formattedNumber,
+      img: Dishwasher,
+      src: "*based on figures by Bosch, 9 things you didn’t know about your dishwasher, 2020.",
+    },
+    trees: {
+      introText:
+        "The Disposable Cups AND is no longer using will contribute to this many trees absoribing CO2 from elsewhere:",
+      figure: formattedTreesNeeded,
+      img: Forest,
+      src: "**based on figures from ecotree.green.",
+    },
+    miles: {
+      introText:
+        "Using a Reusable Cup Has Saved As Much CO2 From The Production of Disposable Cups As Driving This Many Miles:",
+      figure: formattedMilesEquivalent,
+      img: Car,
+      src: "**based on figures from nimblefins.co.uk, using data from Department for Transport, 2022.",
+    },
+  };
+
+  //console.log(`factbank random ${factBankObject.dishwasher.introText}`);
+
+  function chooseFact() {
+    //console.log(factsArray[Math.floor(Math.random() * 3)]);
+    const randItem = ["dishwasher", "trees", "miles"];
+    const rand = Math.floor(Math.random() * 3);
+    const randomFact = randItem[rand];
+    return factBankObject[randomFact];
+  }
+
+  useEffect(() => {
+    const result = chooseFact();
+    setFact(result);
+    console.log(result);
+    fetchClubs();
+    getCupsData();
+  }, []);
+
+  useEffect(() => {
+    if (clubsList) {
+      getDataFromClubs();
+    }
+  }, [clubsList]);
+
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content flex flex-col">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-2xl font-bold">
+            {" "}
+            Welcome Here's How You're Doing:
+          </h1>
+          <div className="flex text-center justify-evenly">
+            <div className="statBackground">
+              <p className="mb-5"> Total ANDis Pledged</p>
+              <p className="mb-5 statNumber"> {cupsData.totalUsers}</p>
+            </div>
+            <div>
+              <p className="mb-5"> Total Cups Saved</p>
+              <p className="mb- statNumber"> {cupsData.totalCupsSaved}</p>
+            </div>
+          </div>
+
+          <div>
+            <h1 className="mb-4">{fact.introText}</h1>
+            <div className="flex justify-evenly items-center">
+              <h2 id="dishwasherX">{fact.figure} In A Year!</h2>
+              <img id="dishwasherImg" src={fact.img} alt="" />
+            </div>
+            <p>{fact.src}</p>
+          </div>
+        </div>
+        <div id="pie-chart">
+          <Pie options={chartOptions} data={chartData} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
