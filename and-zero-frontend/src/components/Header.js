1c6c8c6e299b278e751cf70ff5d04e9585090565
Tia Eastwood <teastwood88@gmail.com>
2022-09-23 10:01:04 +0100

    small fixes

--- and-zero-frontend/src/components/Header.js
+++ and-zero-frontend/src/components/Header.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import AndLogo from "../images/and.png";
 
 function Header() {
 	return (
 		<header className="ANDRed pt-3 pb-3 pl-2 pr-2 flex justify-center text-5xl">
 			<Link to="/">
-				<h1>
+				{/* <h1>
 					<span className="font-bold">AND</span>zero
-				</h1>
+				</h1> */}
+				<img src={AndLogo} alt="and logo" />
 			</Link>
 		</header>
 	);
